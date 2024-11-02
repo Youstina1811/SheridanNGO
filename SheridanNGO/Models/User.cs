2a0eb0f78218b9edab1fbaf49fdcfb983c1ce3b1
Manish Khurana <manishkhrn1@gmail.com>
2024-11-02 19:03:08 -0400

    Added a User Class

---
+++ SheridanNGO/Models/User.cs
@@ -0,0 +1,22 @@
+﻿namespace SheridanNGO.Models
+{
+    public class User
+    {
+        private String Name { get; set; }
+        private String Email { get; set; }
+        private String Password { get; set; }
+        private String Phone { get; set; }
+        private String Address { get; set; }
+      
+        public User(string name, string email, string password, string phone, string address)
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+            Phone = phone;
+            Address = address;
+        }
+
+        public User() { }
+    }
+}
