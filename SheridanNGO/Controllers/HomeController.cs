79546010ba8edb760ed2f3063d06e52719c73fc9
youstinabotros <botrosy@sheridancollege.ca>
2024-11-03 14:48:32 -0500

    Added Index page and Sign in page

--- SheridanNGO/Controllers/HomeController.cs
+++ SheridanNGO/Controllers/HomeController.cs
@@ -5,28 +5,17 @@ using System.Diagnostics;
 namespace SheridanNGO.Controllers
 {
     public class HomeController : Controller
+{
+    public IActionResult Index()
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        return View();
     }
+
+        // Action for Sign In
+        public IActionResult SignIn()
+        {
+            return View(); // This will return the SignIn view
+        }
+
+}
 }
