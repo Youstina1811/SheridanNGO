9857011c78565bfa1d0e86417d91ff2e0e3335f5
petergodot7 <152039813+petergodot7@users.noreply.github.com>
2024-11-25 16:44:37 -0500

    Created Payment controller with a test path /Payment/CreateCharge to test payment implementation currently returns json data since can't do real payment

--- SheridanNGO/Controllers/PaymentController.cs
+++ SheridanNGO/Controllers/PaymentController.cs
@@ -1,12 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Stripe;
 
-namespace SheridanNGO.Controllers
+public class PaymentController : Controller
 {
-    public class PaymentController : Controller
+    private readonly StripeClient _stripeClient;
+
+    public PaymentController(StripeClient stripeClient)
     {
-        public IActionResult Index()
+        _stripeClient = stripeClient;
+    }
+
+    [HttpGet] // Make sure the action is accessible with GET or POST as required
+    public IActionResult CreateCharge()
+    {
+        var options = new PaymentIntentCreateOptions
         {
-            return View();
-        }
+            Amount = 2000, // Amount in cents (e.g., $20.00)
+            Currency = "usd",
+        };
+        var service = new PaymentIntentService(_stripeClient);
+        PaymentIntent intent = service.Create(options);
+
+        // Handle the response and return a simple confirmation or the intent object
+        return Json(intent); // You can return a view or a JSON response for testing
     }
 }
